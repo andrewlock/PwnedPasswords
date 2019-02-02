3f89d29d8e3e25e80e05d4ddffa86fede640cc66
Andrew Lock <andrewlock.net@gmail.com>
2019-02-02 18:25:21 +0000

    Add scripts used to generate

---
+++ info.ps1
@@ -0,0 +1,36 @@
+dotnet PwnedPasswords.FileClient.Loader.dll info "C:\Users\Sock\Downloads\pwned-passwords-v4-1-00001-551509767.filter" -t
+Write-Host ""
+dotnet PwnedPasswords.FileClient.Loader.dll info "C:\Users\Sock\Downloads\pwned-passwords-v4-1-0001-551509767.filter" -t
+Write-Host ""
+dotnet PwnedPasswords.FileClient.Loader.dll info "C:\Users\Sock\Downloads\pwned-passwords-v4-1-001-551509767.filter" -t
+Write-Host ""
+dotnet PwnedPasswords.FileClient.Loader.dll info "C:\Users\Sock\Downloads\pwned-passwords-v4-2-00001-357168428.filter" -t
+Write-Host ""
+dotnet PwnedPasswords.FileClient.Loader.dll info "C:\Users\Sock\Downloads\pwned-passwords-v4-2-0001-357168428.filter" -t
+Write-Host ""
+dotnet PwnedPasswords.FileClient.Loader.dll info "C:\Users\Sock\Downloads\pwned-passwords-v4-2-001-357168428.filter" -t
+Write-Host ""
+dotnet PwnedPasswords.FileClient.Loader.dll info "C:\Users\Sock\Downloads\pwned-passwords-v4-5-00001-90576354.filter" -t
+Write-Host ""
+dotnet PwnedPasswords.FileClient.Loader.dll info "C:\Users\Sock\Downloads\pwned-passwords-v4-5-0001-90576354.filter" -t
+Write-Host ""
+dotnet PwnedPasswords.FileClient.Loader.dll info "C:\Users\Sock\Downloads\pwned-passwords-v4-5-001-90576354.filter" -t
+Write-Host ""
+dotnet PwnedPasswords.FileClient.Loader.dll info "C:\Users\Sock\Downloads\pwned-passwords-v4-10-00001-34403505.filter" -t
+Write-Host ""
+dotnet PwnedPasswords.FileClient.Loader.dll info "C:\Users\Sock\Downloads\pwned-passwords-v4-10-0001-34403505.filter" -t
+Write-Host ""
+dotnet PwnedPasswords.FileClient.Loader.dll info "C:\Users\Sock\Downloads\pwned-passwords-v4-10-001-34403505.filter" -t
+Write-Host ""
+dotnet PwnedPasswords.FileClient.Loader.dll info "C:\Users\Sock\Downloads\pwned-passwords-v4-100-00001-2072328.filter" -t
+Write-Host ""
+dotnet PwnedPasswords.FileClient.Loader.dll info "C:\Users\Sock\Downloads\pwned-passwords-v4-100-0001-2072328.filter" -t
+Write-Host ""
+dotnet PwnedPasswords.FileClient.Loader.dll info "C:\Users\Sock\Downloads\pwned-passwords-v4-100-001-2072328.filter" -t
+Write-Host ""
+dotnet PwnedPasswords.FileClient.Loader.dll info "C:\Users\Sock\Downloads\pwned-passwords-v4-1000-00001-202837.filter" -t
+Write-Host ""
+dotnet PwnedPasswords.FileClient.Loader.dll info "C:\Users\Sock\Downloads\pwned-passwords-v4-1000-0001-202837.filter" -t
+Write-Host ""
+dotnet PwnedPasswords.FileClient.Loader.dll info "C:\Users\Sock\Downloads\pwned-passwords-v4-1000-001-202837.filter" -t
+Write-Host ""
---
+++ verify.ps1
@@ -0,0 +1,36 @@
+dotnet PwnedPasswords.FileClient.Loader.dll verify "C:\Users\Sock\Downloads\pwned-passwords-1-00001-517238891.filter" "C:\Users\Sock\Downloads\pwned-passwords-ordered-by-count.txt" -p 1 -e 0.00001
+Write-Host ""
+dotnet PwnedPasswords.FileClient.Loader.dll verify "C:\Users\Sock\Downloads\pwned-passwords-1-0001-517238891.filter" "C:\Users\Sock\Downloads\pwned-passwords-ordered-by-count.txt" -p 1 -e 0.0001
+Write-Host ""
+dotnet PwnedPasswords.FileClient.Loader.dll verify "C:\Users\Sock\Downloads\pwned-passwords-1-001-517238891.filter" "C:\Users\Sock\Downloads\pwned-passwords-ordered-by-count.txt" -p 1 -e 0.001
+Write-Host ""
+dotnet PwnedPasswords.FileClient.Loader.dll verify "C:\Users\Sock\Downloads\pwned-passwords-2-00001-344791361.filter" "C:\Users\Sock\Downloads\pwned-passwords-ordered-by-count.txt" -p 2 -e 0.00001
+Write-Host ""
+dotnet PwnedPasswords.FileClient.Loader.dll verify "C:\Users\Sock\Downloads\pwned-passwords-2-0001-344791361.filter" "C:\Users\Sock\Downloads\pwned-passwords-ordered-by-count.txt" -p 2 -e 0.0001
+Write-Host ""
+dotnet PwnedPasswords.FileClient.Loader.dll verify "C:\Users\Sock\Downloads\pwned-passwords-2-001-344791361.filter" "C:\Users\Sock\Downloads\pwned-passwords-ordered-by-count.txt" -p 2 -e 0.001
+Write-Host ""
+dotnet PwnedPasswords.FileClient.Loader.dll verify "C:\Users\Sock\Downloads\pwned-passwords-5-00001-87246372.filter" "C:\Users\Sock\Downloads\pwned-passwords-ordered-by-count.txt" -p 5 -e 0.00001
+Write-Host ""
+dotnet PwnedPasswords.FileClient.Loader.dll verify "C:\Users\Sock\Downloads\pwned-passwords-5-0001-87246372.filter" "C:\Users\Sock\Downloads\pwned-passwords-ordered-by-count.txt" -p 5 -e 0.0001
+Write-Host ""
+dotnet PwnedPasswords.FileClient.Loader.dll verify "C:\Users\Sock\Downloads\pwned-passwords-5-001-87246372.filter" "C:\Users\Sock\Downloads\pwned-passwords-ordered-by-count.txt" -p 5 -e 0.001
+Write-Host ""
+dotnet PwnedPasswords.FileClient.Loader.dll verify "C:\Users\Sock\Downloads\pwned-passwords-10-00001-33256195.filter" "C:\Users\Sock\Downloads\pwned-passwords-ordered-by-count.txt" -p 10 -e 0.00001
+Write-Host ""
+dotnet PwnedPasswords.FileClient.Loader.dll verify "C:\Users\Sock\Downloads\pwned-passwords-10-0001-33256195.filter" "C:\Users\Sock\Downloads\pwned-passwords-ordered-by-count.txt" -p 10 -e 0.0001
+Write-Host ""
+dotnet PwnedPasswords.FileClient.Loader.dll verify "C:\Users\Sock\Downloads\pwned-passwords-10-001-33256195.filter" "C:\Users\Sock\Downloads\pwned-passwords-ordered-by-count.txt" -p 10 -e 0.001
+Write-Host ""
+dotnet PwnedPasswords.FileClient.Loader.dll verify "C:\Users\Sock\Downloads\pwned-passwords-100-00001-2005322.filter" "C:\Users\Sock\Downloads\pwned-passwords-ordered-by-count.txt" -p 100 -e 0.00001
+Write-Host ""
+dotnet PwnedPasswords.FileClient.Loader.dll verify "C:\Users\Sock\Downloads\pwned-passwords-100-0001-2005322.filter" "C:\Users\Sock\Downloads\pwned-passwords-ordered-by-count.txt" -p 100 -e 0.0001
+Write-Host ""
+dotnet PwnedPasswords.FileClient.Loader.dll verify "C:\Users\Sock\Downloads\pwned-passwords-100-001-2005322.filter" "C:\Users\Sock\Downloads\pwned-passwords-ordered-by-count.txt" -p 100 -e 0.001
+Write-Host ""
+dotnet PwnedPasswords.FileClient.Loader.dll verify "C:\Users\Sock\Downloads\pwned-passwords-1000-00001-198624.filter" "C:\Users\Sock\Downloads\pwned-passwords-ordered-by-count.txt" -p 1000 -e 0.00001
+Write-Host ""
+dotnet PwnedPasswords.FileClient.Loader.dll verify "C:\Users\Sock\Downloads\pwned-passwords-1000-0001-198624.filter" "C:\Users\Sock\Downloads\pwned-passwords-ordered-by-count.txt" -p 1000 -e 0.0001
+Write-Host ""
+dotnet PwnedPasswords.FileClient.Loader.dll verify "C:\Users\Sock\Downloads\pwned-passwords-1000-001-198624.filter" "C:\Users\Sock\Downloads\pwned-passwords-ordered-by-count.txt" -p 1000 -e 0.001
+Write-Host ""
